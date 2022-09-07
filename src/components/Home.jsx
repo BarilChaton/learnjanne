02ea566970b304b7cdc8f9ca55c8831e42702628
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-09-07 15:12:35 +0200

    initial

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { GoSmiley } from 'react-icons/go'
+
+const Home = () => {
+  return (
+    <div className='w-screen h-screen'>
+        <div className='h-full flex flex-col justify-center items-center text-[59em] animate-spin-slow'>
+            <GoSmiley />
+        </div>
+    </div>
+  )
+}
+
+export default Home
